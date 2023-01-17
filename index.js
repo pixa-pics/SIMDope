7a491ade0eaf9229f1fc14d03d316ca383cd76c2
Vipertech <74778466+vipertechofficial@users.noreply.github.com>
2023-01-17 11:33:19 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2022 - 2022 Matias Affolter
+Copyright (c) 2022 - 2023 Matias Affolter
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
